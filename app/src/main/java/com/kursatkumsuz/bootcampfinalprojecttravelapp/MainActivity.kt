39cef77a64bb1531cd5cb3e2d71290840238ba86
DrHistory <kursatkumsuz@gmail.com>
2022-09-29 13:28:01 +0300

    add BottomNavigationView + FragmentContainerView

--- app/src/main/java/com/kursatkumsuz/bootcampfinalprojecttravelapp/MainActivity.kt
+++ app/src/main/java/com/kursatkumsuz/bootcampfinalprojecttravelapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.kursatkumsuz.bootcampfinalprojecttravelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.kursatkumsuz.bootcampfinalprojecttravelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navHostFragment()
+    }
+
+    private fun navHostFragment() {
+        binding.apply {
+            val navHostFragment  = supportFragmentManager.findFragmentById(fragmentContainerView.id) as NavHostFragment
+            NavigationUI.setupWithNavController(bottomNavigationView,navHostFragment.navController)
+        }
     }
 }
