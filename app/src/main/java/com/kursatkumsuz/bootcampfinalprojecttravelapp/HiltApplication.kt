51855c437463bded0c89fbc1a0294e71a7bdde48
DrHistory <kursatkumsuz@gmail.com>
2022-09-30 08:28:23 +0300

    add Dependency Injection

---
+++ app/src/main/java/com/kursatkumsuz/bootcampfinalprojecttravelapp/HiltApplication.kt
@@ -0,0 +1,7 @@
+package com.kursatkumsuz.bootcampfinalprojecttravelapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application()
